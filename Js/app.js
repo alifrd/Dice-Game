0a95ef8a7db2ff726332a8468193ae7309852b9e
ali frd <alifrd49@gmail.com>
2018-07-29 17:01:24 +0430

    added btn_hold

--- Js/app.js
+++ Js/app.js
@@ -22,12 +22,30 @@ document.querySelector(".btn-roll").addEventListener("click", function () {
 	diceDom.style.display = 'block';	
 	diceDom.src = "Images/dice-"+dice+".png";
 
-	if (dice !== 1) {
-
+	if (dice !== 1){
+		roundScore +=dice;
+		document.querySelector("#current-"+activePleyer).textContent = roundScore;
 	}
 	else{
-
+		Next_pleyer();
 	}
-}) 
 
-console.log(dice);
+});
+
+
+document.querySelector(".btn-hold").addEventListener("click",function () {
+	scores[activePleyer] += roundScore;
+	document.querySelector("#score-"+activePleyer).textContent = scores[activePleyer] ;
+	Next_pleyer();
+
+} );
+
+function Next_pleyer() {
+	activePleyer ===0 ? activePleyer = 1 : activePleyer = 0 ; 
+	roundScore = 0;
+	document.getElementById("current-0").textContent = 0 ;
+	document.getElementById("current-1").textContent = 0 ;
+	document.querySelector(".player-1-panel").classList.toggle("active");
+	document.querySelector(".player-0-panel").classList.toggle("active");
+	document.querySelector(".dice").style.display = 'none';			
+}
