f383e9f6ee1d6e869d3e45d2c037449a5b502e2d
ali frd <alifrd49@gmail.com>
2018-07-30 18:11:01 +0430

    Finish Vresion One

--- Js/app.js
+++ Js/app.js
@@ -59,6 +59,6 @@ function init() {
 	document.getElementById("score-1").textContent = 0 ;
 	document.getElementById("current-0").textContent = 0 ;
 	document.getElementById("current-1").textContent = 0 ;
-	document.getElementById("name-0").textContent = "Player 1";
-	document.getElementById("name-1").textContent = "Player 2";
+	document.getElementById("name-0").innerHTML = "Player 1";
+	document.getElementById("name-1").innerHTML	= "Player 2";
 }
